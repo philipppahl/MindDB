96c58b9eaf128aed28b728c04d80b2392c05aa25
Philipp Pahl <ph.pahl@gmail.com>
2025-02-06 15:26:50 +0000

    add note and import add-on for Anki

---
+++ add-ons/create_custom_note_type/__init__.py
@@ -0,0 +1,104 @@
+from anki.models import NotetypeDict
+from aqt.gui_hooks import main_window_did_init
+from aqt import mw
+from aqt.utils import showInfo
+from aqt.qt import qconnect
+import time
+
+
+def create_multiple_choice():
+    model_manager = mw.col.models
+    existing_model = model_manager.byName("Multiple Choice")
+    if existing_model:
+        showInfo("The 'Multiple Choice' note type already exists.")
+        return
+
+    question_format = (
+        "{{Question}}\n{{Answer1}}\n{{Answer2}}\n{{Answer3}}\n{{Answer4}}"
+    )
+    answer_format = (
+        "{{FrontSide}}\n{{Question}}\n{{Answer1}}\n{{Answer2}}\n{{Answer3}}\n"
+        "{{Answer4}}\nCorrect Answer: {{CorrectAnswer}}\n{{Explanation}}"
+    )
+
+    def create_field(name: str, ord: int) -> dict:
+        """Create a field definition with default properties."""
+        return {
+            "name": name,
+            "ord": ord,
+            "sticky": False,
+            "rtl": False,
+            "font": "Arial",
+            "size": 12,
+            "media": [],
+            "plainText": True,
+            "collapsed": False,
+            "excludeFromSearch": False
+        }
+
+    field_names = [
+        "NoteID",
+        "Question",
+        "Answer1",
+        "Answer2",
+        "Answer3",
+        "Answer4",
+        "CorrectAnswer",
+        "Explanation"
+    ]
+
+    # Define the new note type, aka model
+    model = NotetypeDict(
+        name="Multiple Choice",
+        type=0,
+        id=0,
+        mod=int(time.time()),
+        usn=-1,
+        did=1,  # Default deck
+        flds=[create_field(name, i) for i, name in enumerate(field_names)],
+        tmpls=[{
+            "name": "Multiple Choice Card",
+            "qfmt": question_format,
+            "afmt": answer_format,
+            "bqfmt": "",
+            "bafmt": "",
+            "did": None,
+            "ord": 0,
+            "bfont": "Arial",
+            "bsize": 12
+        }],
+        css="""
+        .explanation { font-style: italic; }
+        .note-id {
+            font-size: 0.8em;
+            color: #666;
+            margin-bottom: 8px;
+        }
+        """,
+        latexPre=(
+            "\\documentclass[12pt]{article}\n\\special{papersize=3in,5in}\n\\"
+            "usepackage[utf8]{inputenc}\n\\usepackage{amssymb,amsmath}\n\\"
+            "pagestyle{empty}\n\\setlength{\\parindent}{0in}\n\\"
+            "begin{document}\n"
+        ),
+        latexPost="\\end{document}",
+        latexsvg=False,
+        req=[],
+        sortf=0,
+        vers=[]
+    )
+
+    # Add the new note type to the collection
+    model_manager.add(model)
+    model_manager.update(model)
+    showInfo("New note type 'Multiple Choice' has been added.")
+
+
+# Hook into Anki's GUI to add a menu option
+def setup_menu():
+    add_action_msg = "Create Multiple Choice Note Type"
+    action = mw.form.menuTools.addAction(add_action_msg)
+    qconnect(action.triggered, create_multiple_choice)
+
+
+main_window_did_init.append(setup_menu)
